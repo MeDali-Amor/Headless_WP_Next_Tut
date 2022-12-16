8650c64b41b3973e7e60a556499801d89219f457
Med Ali Amor <amormedali92@gmail.com>
2022-12-16 15:49:31 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    trailingSlash: false,
+    // reactStrictMode: true,
+    // swcMinify: true,
+    webpackDevMiddleware: (config) => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
 
-module.exports = nextConfig
+        return config;
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+    },
+};
+
+module.exports = nextConfig;
